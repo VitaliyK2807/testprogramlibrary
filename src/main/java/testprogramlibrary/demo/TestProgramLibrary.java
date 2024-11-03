6890eafae586c5f3b18f31fd1b1c6c60c1c972a5
Vitaliy <Slesar_v@mail.ru>
2024-11-03 10:45:36 +0700

    New program

---
+++ src/main/java/testprogramlibrary/demo/TestProgramLibrary.java
@@ -0,0 +1,13 @@
+package testprogramlibrary.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProgramLibrary {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProgramLibrary.class, args);
+	}
+
+}
---
+++ src/test/java/testprogramlibrary/demo/TestProgramLibraryTests.java
@@ -0,0 +1,13 @@
+package testprogramlibrary.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProgramLibraryTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
